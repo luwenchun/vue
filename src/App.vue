669cd1f5b3186599296b6dcaee4919eee2d30419
luwenchun <luwenchun@users.noreply.github.com>
2019-05-05 12:55:51 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,28 @@
     <p>
       <input type="text" v-model="text">
     </p>
+        <!-- 循环语句 -->
+    <ul>
+      <li v-for="(good, index) in goods" :key="good.id">
+        <span>{{good.text}}</span>
+        <span>￥{{good.price}}</span>
+        <button @click="addGood(index)">加购物车</button>
+      </li>
+    </ul>
+      <!-- 购物车 -->
+    <cart :name="name"></cart>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+// import HelloWorld from "./components/HelloWorld.vue";
+import Cart from "./components/Cart.vue";
+import axios from "axios";
 export default {
   name: "app",
+   components: {
+    Cart
+  },
   data() {
     return {
       name: "开课吧购物车",
@@ -23,9 +37,37 @@ export default {
       text: ""
     };
   },
-  components: {
-    HelloWorld
-  }
+    async created() {
+    // 创建钩子，组件创建完成执行一次
+    setTimeout(() => {
+      this.showName = true;
+    }, 1000);
+
+    // 查询产品列表
+    try {
+      const response = await axios.get("/api/goods");
+      console.log(response);
+      this.goods = response.data.list;
+    } catch (error) {
+      // 错误处理
+    }
+
+    // 不用await
+    // axios.get("/api/goods").then(resp => {
+    //   this.goods = response.data.list;
+    // }).catch(err => {
+    //   // 错误处理
+    // })
+  },
+   methods: {
+    addGood(i) {
+      // 获取goods中对应项
+      const good = this.goods[i];
+      this.$bus.$emit("addCart", good);
+      // this.$root.$emit('addCart', good)
+    }
+  },
+ 
 };
 </script>
 
--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -32,6 +32,71 @@
   </div>
 </template>
 <script>
+export default {
+    props: ["name"],
+  data() {
+    return {
+      cart: []
+    };
+  },
+    created() {
+    // 监听一下父组件添加商品事件
+    this.$bus.$on("addCart", good => {
+      const ret = this.cart.find(v => v.id === good.id);
+      if (ret) {
+        // 购物车里已有该商品
+        ret.count += 1;
+      } else {
+        this.cart.push({
+          ...good, // 属性展开
+          count: 1,
+          active: true
+        });
+      }
+    });
+  },
+    methods: {
+    minus(i) {
+      const count = this.cart[i].count;
+      if (count > 1) {
+        this.cart[i].count -= 1;
+      } else {
+        this.remove(i);
+      }
+    },
+    add(i) {
+      this.cart[i].count += 1;
+    },
+    remove(i) {
+      if (window.confirm("确定删除？")) {
+        this.cart.splice(i, 1);
+      }
+    }
+  },
+   computed: {
+    activeCount() {
+      // 过滤出激活项的数量
+      return this.cart.filter(v => v.active).length;
+    },
+    count() {
+      // 购物车项总数
+      return this.cart.length;
+    },
+    total() {
+      //计算激活项总价
+      let num = 0;
+      this.cart.forEach(c => {
+        if (c.active) {
+          num += c.price * c.count;
+        }
+      });
+      return num;
+    }
+  }
+}
 </script>
 <style scope>
+.active {
+  color: green;
+}
 </style>
