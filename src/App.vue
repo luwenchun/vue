7ade79c50a188b56a3319c3ede31a8eb85895eb8
luwenchun <luwenchun@users.noreply.github.com>
2019-05-05 12:55:37 +0800

    vue-cli初始demo 1：$bus解决多层组件传值问题 2:  this.cart.find(v => v.id === good.id);找到已经存在的对象，并返回当前对象 3  this.cart.filter(v => v.active).length；返回找到符合条件对象的集合 4 async created 创建钩子，组件创建完成执行一次

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- 条件语句 -->
+    <p v-if="showName">{{name}}</p>
+    <p>
+      <input type="text" v-model="text">
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      name: "开课吧购物车",
+      showName: false,
+      goods: [],
+      text: ""
+    };
+  },
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Cart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{name}}</p>
+    <!-- 购物车列表 -->
+    <table border="1">
+      <tr>
+        <th>#</th>
+        <th>课程名</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>价格</th>
+      </tr>
+      <tr v-for="(c,i) in cart" :key="c.id" :class="{active:c.active}">
+        <td>
+          <input type="checkbox" v-model="c.active">
+        </td>
+        <td>{{c.text}}</td>
+        <td>￥{{c.price}}</td>
+        <td>
+          <button @click="minus(i)">-</button>
+          {{c.count}}
+          <button @click="add(i)">+</button>
+        </td>
+        <td>￥{{c.price*c.count}}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="2">{{activeCount}}/{{count}}</td>
+        <td colspan="2">￥{{total}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+</script>
+<style scope>
+</style>
