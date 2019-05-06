79351e978a21df22cb6399314c1b1ace926a4f7a
luwenchun <luwenchun@users.noreply.github.com>
2019-05-06 14:13:34 +0800

    解决动画不能重复的问题，通过组件的销毁和从新实例化解决

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,7 @@
     <!-- 商品分类列表 -->
     <cube-drawer ref="drawer" title="请选择分类" :data="[drawerList]" @select="selectHandler"></cube-drawer>
     <!-- 加购动画载体 -->
-    <div class="ball-wrap">
+    <!-- <div class="ball-wrap">
       <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
         <div class="ball" v-show="ball.show">
           <div class="inner">
@@ -26,12 +26,14 @@
           </div>
         </div>
       </transition>
-    </div>
+    </div>-->
   </div>
 </template>
 
 <script>
 import GoodsList from "@/components/GoodsList.vue";
+import create from "@/components/create";
+import BallAnim from "@/components/BallAnim.vue";
 import { debuglog } from "util";
 
 const labels = {
@@ -81,44 +83,52 @@ export default {
       this.selectedKeys = [...val];
     },
     onAddCart(el) {
-      this.ball.el = el;
-      this.ball.show = true; // 触发动画钩子
-    },
-    beforeEnter(el) {
-      // 设置小球初始位置
-      // 小球移动到点击的位置
-      // 1. 获取点击的dom位置
-      const dom = this.ball.el;
-
-      const rect = dom.getBoundingClientRect();
-      console.log(rect.top, rect.left);
-      // 2. 把小球从终点移动到点击的位置
-      const x = rect.left - window.innerWidth / 2;
-      const y = -(window.innerHeight - rect.top - 10 - 20);
-      el.style.display = "block";
-      // ball只移动y
-      // el.style.transform = `translate3d(${x}px, ${y}px, 0)`;
-      el.style.transform = `translate3d(0, ${y}px, 0)`;
-      const inner = el.querySelector(".inner");
-      // inner只移动x
-      inner.style.transform = `translate3d(${x}px,0,0)`;
-    },
-    enter(el, done) {
-      // 把小球移动到初始位置 加上动画
-      // 获取offsetHeight就会重绘，前面的变量名随意 主要为了eslint校验
-      document.body.offsetHeight;
-      // 动画开始，移动到初始位置
-      // 小球移动到购物车位置
-      el.style.transform = `translate3d(0, 0, 0)`;
-      const inner = el.querySelector(".inner");
-      inner.style.transform = `translate3d(0,0,0)`;
-      el.addEventListener("transitionend", done);
-    },
-    afterEnter(el) {
-      // 结束 隐藏小球
-      this.ball.show = true;
-      el.style.display = "true";
+      // this.ball.el = el;
+      // this.ball.show = true; // 触发动画钩子
+      // 手动创建组件实例
+      const anim = create(BallAnim, { el });
+      anim.start();
+      anim.$on("transitionend", () => {
+        anim.remove();
+      });
     }
+    // beforeEnter(el) {
+    //   // 设置小球初始位置
+    //   // 小球移动到点击的位置
+    //   // 1. 获取点击的dom位置
+    //   const dom = this.ball.el;
+
+    //   const rect = dom.getBoundingClientRect();
+    //   console.log(rect.top, rect.left);
+    //   // 2. 把小球从终点移动到点击的位置
+    //   const x = rect.left - window.innerWidth / 2;
+    //   const y = -(window.innerHeight - rect.top - 10 - 20);
+    //   el.style.display = "block";
+    //   // ball只移动y
+    //   // el.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+    //   el.style.transform = `translate3d(0, ${y}px, 0)`;
+    //   const inner = el.querySelector(".inner");
+    //   // inner只移动x
+    //   inner.style.transform = `translate3d(${x}px,0,0)`;
+    // },
+    // enter(el, done) {
+    //   // 把小球移动到初始位置 加上动画
+    //   // 获取offsetHeight就会重绘，前面的变量名随意 主要为了eslint校验
+    //   document.body.offsetHeight;
+    //   // 动画开始，移动到初始位置
+    //   // 小球移动到购物车位置
+    //   el.style.transform = `translate3d(0, 0, 0)`;
+    //   const inner = el.querySelector(".inner");
+    //   inner.style.transform = `translate3d(0,0,0)`;
+    //   el.addEventListener("transitionend", done);
+    // },
+    // afterEnter(el) {
+    //   // 结束 隐藏小球
+    //   this.ball.show = true;
+    //   el.style.display = "true";
+    //   // 派发事件
+    //   this.$emit("transitionend");
+    // }
   },
   computed: {
     filterGoods() {
